f789bcde6f33861a45a7a6d09dfab87e137bd162
Sai Sravanthi Ponigate <sravanthiponigate@gmail.com>
2021-05-08 14:07:35 +0530

    Update Practice.java

--- practice/src/practice/Practice.java
+++ practice/src/practice/Practice.java
@@ -1,5 +1,18 @@
 package practice;
 
 public class Practice {
-
+	public static void main(String[] args) {
+    //0123456789
+	String s1="hi how are";//how-->(s,d)(3,6)
+//charAt is used to find the character at a particular location
+	System.out.println(s1.charAt(3));
+//length
+	System.out.println(s1.length());
+//indexOf
+	System.out.println(s1.indexOf('h'));
+//lastIndexOf
+	System.out.println(s1.lastIndexOf('h'));
+//toUpperCase() ,toLowerCase()
+	System.out.println(s1.toUpperCase()+" "+s1.toLowerCase());
+  }
 }
